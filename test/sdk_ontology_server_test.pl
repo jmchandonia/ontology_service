3855e55964cc25b04d1cbc0a9890111fdb012192
John-Marc Chandonia <JMChandonia@lbl.gov>
2017-04-06 21:43:07 +0000

    set auth url in token creation in test

--- test/sdk_ontology_server_test.pl
+++ test/sdk_ontology_server_test.pl
@@ -14,7 +14,7 @@ my $config = new Config::Simple($config_file)->get_block('sdk_ontology');
 my $ws_url = $config->{"workspace-url"};
 my $ws_name = undef;
 my $ws_client = new Bio::KBase::workspace::Client($ws_url,token => $token);
-my $auth_token = Bio::KBase::AuthToken->new(token => $token, ignore_authrc => 1);
+my $auth_token = Bio::KBase::AuthToken->new(token => $token, ignore_authrc => 1, auth_svc=>$config->{'auth-service-url'});
 my $ctx = LocalCallContext->new($token, $auth_token->user_id);
 $sdk_ontology::sdk_ontologyServer::CallContext = $ctx;
 my $impl = new sdk_ontology::sdk_ontologyImpl();
